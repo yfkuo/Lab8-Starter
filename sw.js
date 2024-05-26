8658112ecbacf64eebcf7e807a7d7421818b36eb
yfkuo <yfkuo@ucsd.edu>
2024-05-26 16:15:42 -0700

    "test"

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 // sw.js - This file needs to be in the root of the directory to work,
 //         so do not move it next to the other scripts
-
 const CACHE_NAME = 'lab-8-starter';
-const RECIPE_URLs = [
+const RECIPE_URLS = [
   'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
   'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
   'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
@@ -13,11 +12,15 @@ const RECIPE_URLs = [
 
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
+
+  console.log('Service Worker: Installing...');
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll(RECIPE_URLs);
+      console.log('Service Worker: Caching core assets and recipe URLs');
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -29,6 +32,7 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
+  console.log('Service Worker: Fetch event for ', event.request.url);
   // We added some known URLs to the cache above, but tracking down every
   // subsequent network request URL and adding it manually would be very taxing.
   // We will be adding all of the resources not specified in the intiial cache
@@ -42,15 +46,24 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
-    return cache.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
-        cache.put(event.request, fetchedResponse.clone());
-        return fetchedResponse;
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('return cache.match(event.request).then(function (response)');
+      return cache.match(event.request).then(function (response) {
+      // B8. TODO - If the request is in the cache, return with the cached version.
+      //            Otherwise fetch the resource, add it to the cache, and return
+      //            network response.
+        if (response) {
+          console.log('return (response)');
+          return response;
+        } else {
+          fetch(event.request).then(function (networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            console.log('return networkResponse');
+            return networkResponse;
+          });
+        }
       });
-    });
-  }));
+    })
+  );
 });
